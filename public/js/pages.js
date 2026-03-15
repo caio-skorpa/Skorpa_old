9e9e64ec9f38a678f4fdb6bcd0989fe7377bff63
Caio Gomes Fontoura <ggomescaio@gmail.com>
2026-03-15 18:37:07 -0300

    Melhorias

--- public/js/pages.js
+++ public/js/pages.js
@@ -20,3 +20,47 @@ closeButtons.forEach(e => {
         modal.close();
     });
 });
+
+
+
+
+const input = document.querySelector("#asset-search");
+const list = document.querySelector("#options-list");
+const options = list.querySelectorAll("li");
+
+input.addEventListener("input", () => {
+
+    const value = input.value.toLowerCase();
+
+    if (value === "") {
+        list.style.display = "none";
+        return;
+    }
+
+    let visible = false;
+
+    options.forEach(option => {
+
+        const text = option.textContent.toLowerCase();
+
+        option.style.display = "block";
+
+        if (!text.includes(value)) {
+            option.style.display = "none";
+        } else {
+            visible = true;
+        }
+
+    });
+
+    list.style.display = visible ? "block" : "none";
+});
+
+options.forEach(option => {
+    option.addEventListener("click", () => {
+
+        input.value = option.textContent; // coloca no input
+        list.style.display = "none"; // fecha a lista
+
+    });
+});
