04107bfdfc8f0f4fd14d1c9308ed841f6ee31845
Caio Gomes Fontoura <ggomescaio@gmail.com>
2026-03-24 13:23:47 -0300

    Implementando API Brapi.dev

--- src/api/brapi.js
+++ src/api/brapi.js
@@ -1,3 +1,19 @@
 import "dotenv/config";
+import Brapi from "brapi";
 
 const BP_API_KEY = process.env.BRAPI_API_KEY;
+
+const client = new Brapi({
+    apiKey: BP_API_KEY,
+});
+
+try {
+    const quote = await client.quote.retrieve("BBSE3");
+    console.log(quote)
+} catch (error) {
+    if (error instanceof Brapi.RateLimitError) {
+        // Trate limite de requisições
+    } else if (error instanceof Brapi.NotFoundError) {
+        // Trate ticker não encontrado
+    }
+}
